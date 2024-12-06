cedbffcff0b00e7764b49ebd423526f94c0673e2
CodenameNEXUS <puggaming456@gmail.com>
2024-12-06 13:57:06 -0800

    adding and updating shoot script

---
+++ MobileGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+    private Transform target;
+
+    [Header("Attriutes")]
+    [SerializeField] private float bulletSpeed = 5f; //ignore: 6 minutes into vid
+
+}
