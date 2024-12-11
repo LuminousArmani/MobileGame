33bcda85c259eb52c227fecdc31f922d63d690dd
CodenameNEXUS <puggaming456@gmail.com>
2024-12-11 12:53:04 -0800

    adding UI to TD mode

--- MobileGame/Assets/Scripts/BuildManager.cs
+++ MobileGame/Assets/Scripts/BuildManager.cs
@@ -5,18 +5,17 @@ public class BuildManager : MonoBehaviour
     public static BuildManager main;
 
     [Header("References")]
-    [SerializeField] private GameObject[] towerPrefabs;
+    //[SerializeField] private GameObject[] towerPrefabs;
+    [SerializeField] private Tower[] towers; 
 
     private int selectedTower = 0;
-
-
 
     private void Awake()
     {
         main = this;
     }
-    public GameObject GetSelectedTower()
+    public Tower GetSelectedTower()
     {
-        return towerPrefabs[selectedTower];
+        return towers[selectedTower];
     }
 }
