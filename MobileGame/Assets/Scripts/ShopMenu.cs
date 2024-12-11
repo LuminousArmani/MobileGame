4dfe3472fe96e52025653be31c2cd7b9ceb7f487
CodenameNEXUS <puggaming456@gmail.com>
2024-12-11 13:56:42 -0800

    updated TD Menu

--- MobileGame/Assets/Scripts/ShopMenu.cs
+++ MobileGame/Assets/Scripts/ShopMenu.cs
@@ -1,24 +1,32 @@
 using UnityEngine;
 using TMPro;
+using UnityEditor;
 
 public class ShopMenu : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] TextMeshProUGUI currencyUI;
+    [SerializeField] Animator anim;
+
+    private bool isMenuOpen = true;
+
+    public void ToggleMenu()
+    {
+        isMenuOpen = !isMenuOpen;
+        anim.SetBool("MenuOpen", isMenuOpen);
+
+
+
+
+
+
+
+
+
+    }
 
     private void OnGUI()
     {
         currencyUI.text = LevelManager.main.power.ToString();
     }
-
-    public void SetSelected()
-    {
-
-    }
-
-
-
-
-
-
 }
